0420e71b2807936b56bf28adffc93155b24a2c6e
Pablo Carrazana <pablo.carrazana@claro.com.ar>
2020-12-13 12:45:31 -0300

    add file logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './logoClaro.png';
 import './App.css';
+
+// function Hello(props) {
+// return <h2>{props.title}</h2>
+// }
+
+// const Hello = (props) => <h2>{props.title}</h2>
+
+class Hello extends Component{
+  render(){
+    return <h2>{this.props.title}</h2>
+  }
+}
+
+class Text extends Component{
+  render(){
+    const textoSegunBool = this.props.isActivated ? 'On' : 'Off'
+    const mappedNumbers = this.props.arrayOfNumbers.map( n => n * 2)
+    return(
+      <div>
+      <p>{mappedNumbers.join(', ')}</p>
+      <p>{this.props.objectWithInfo.key}</p>
+      </div>
+    )
+    
+    
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Hello title = "Bienvenido a este curso" />
+
+        <Text number = {2} 
+        text = "Vamos a aprender React" 
+        isActivated
+        arrayOfNumbers = {[2,3,10]}
+        objectWithInfo = {{key: 'value' ,key2: 'otherValue'}} 
+        />
+
+        <p>&#128512;</p>
+        <table>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Edad</th>
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+          </tr>
+        </table>
+
         <a
           className="App-link"
           href="https://reactjs.org"
